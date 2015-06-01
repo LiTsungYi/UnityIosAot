2653f94dd4929099ab1f339844c0121552f42720
litsungyi <litsungyi@gmail.com>
2015-06-02 00:21:20 +0800

    First commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+        var test = new AotTest();
+        test.GenericA<int>();
+        test.GenericB<int>();
+        test.ReflectionC<int>();
+        test.ReflectionD<int>();
+    }
+
+    void Update()
+    {
+
+    }
+}
